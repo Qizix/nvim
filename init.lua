f3abc527738990e0023fc9cdef0a7b4e65a17fe1
JJ_EMPTY_STRING <JJ_EMPTY_STRING>
2026-01-31 10:40:46 +0200

    migrate jdhao conf into my conf

--- init.lua
+++ init.lua
@@ -1,100 +1,58 @@
-if vim.g.vscode then
-  vim.schedule(function()
-    vim.o.clipboard = 'unnamedplus'
-  end)
+  -- Break inserted text into smaller undo units when we insert some punctuation chars.
+  local undo_ch = { ",", ".", "!", "?", ";", ":" }
+  for _, ch in ipairs(undo_ch) do
+    keymap.set("i", ch, ch .. "<c-g>u")
+  end
 
-  -- Enable break indent
-  vim.o.breakindent = true
+  -- Do not move my cursor when joining lines.
+  keymap.set("n", "J", function()
+    vim.cmd([[
+        normal! mzJ`z
+        delmarks z
+      ]])
+  end, {
+    desc = "join lines without moving cursor",
+  })
 
-  -- Save undo history
-  vim.o.undofile = true
-else
-  --[[
+  -- Replace visual selection with text in register, but not contaminate the register,
+  -- see also https://stackoverflow.com/q/10723700/6064933.
+  keymap.set("x", "p", '"_c<Esc>p')
 
-	=====================================================================
-	==================== READ THIS BEFORE CONTINUING ====================
-	=====================================================================
-	========                                    .-----.          ========
-	========         .----------------------.   | === |          ========
-	========         |.-""""""""""""""""""-.|   |-----|          ========
-	========         ||                    ||   | === |          ========
-	========         ||   KICKSTART.NVIM   ||   |-----|          ========
-	========         ||                    ||   | === |          ========
-	========         ||                    ||   |-----|          ========
-	========         ||:Tutor              ||   |:::::|          ========
-	========         |'-..................-'|   |____o|          ========
-	========         `"")----------------(""`   ___________      ========
-	========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-	========       /:::========|  |==hjkl==:::\  \ required \    ========
-	========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-	========                                                     ========
-	=====================================================================
-	=====================================================================
+  -- Copy entire buffer.
+  keymap.set("n", "<leader>y", "<cmd>%yank<cr>", { desc = "yank entire buffer" })
 
-	What is Kickstart?
+  -- Remove trailing whitespace characters
+  keymap.set("n", "<leader><space>", "<cmd>StripTrailingWhitespace<cr>", { desc = "remove trailing space" })
 
-	  Kickstart.nvim is *not* a distribution.
+  -- Reselect the text that has just been pasted, see also https://stackoverflow.com/a/4317090/6064933.
+  keymap.set("n", "<leader>v", "printf('`[%s`]', getregtype()[0])", {
+    expr = true,
+    desc = "reselect last pasted area",
+  })
 
-	  Kickstart.nvim is a starting point for your own configuration.
-	    The goal is that you can read every line of code, top-to-bottom, understand
-	    what your configuration is doing, and modify it to suit your needs.
+  -- Continuous visual shifting (does not exit Visual mode), `gv` means
+  -- to reselect previous visual area, see https://superuser.com/q/310417/736190
+  keymap.set("x", "<", "<gv")
+  keymap.set("x", ">", ">gv")
 
-	    Once you've done that, you can start exploring, configuring and tinkering to
-	    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-	    or immediately breaking it into modular pieces. It's up to you!
+  -- Go to start or end of line easier
+  keymap.set({ "n", "x" }, "H", "^")
+  keymap.set({ "n", "x" }, "L", "g_")
 
-	    If you don't know anything about Lua, I recommend taking some time to read through
-	    a guide. One possible example which will only take 10-15 minutes:
-	      - https://learnxinyminutes.com/docs/lua/
+  keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+  keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+  keymap.set("n", "^", "g^")
+  keymap.set("n", "0", "g0")
 
-	    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-	    reference for how Neovim integrates Lua.
-	    - :help lua-guide
-	    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
+  keymap.set("n", "<space>o", "printf('m`%so<ESC>``', v:count1)", {
+    expr = true,
+    desc = "insert line below",
+  })
 
-	Kickstart Guide:
-
-	  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-	    If you don't know what this means, type the following:
-	      - <escape key>
-	      - :
-	      - Tutor
-	      - <enter key>
-
-	    (If you already know the Neovim basics, you can skip this step.)
-
-	  Once you've completed that, you can continue working through **AND READING** the rest
-	  of the kickstart init.lua.
-
-	  Next, run AND READ `:help`.
-	    This will open up a help window with some basic information
-	    about reading, navigating and searching the builtin help documentation.
-
-	    This should be the first place you go to look when you're stuck or confused
-	    with something. It's one of my favorite Neovim features.
-
-	    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-	    which is very useful when you're not exactly sure of what you're looking for.
-
-	  I have left several `:help X` comments throughout the init.lua
-	    These are hints about where to find more information about the relevant settings,
-	    plugins or Neovim features used in Kickstart.
-
-	   NOTE: Look for lines like this
-
-	    Throughout the file. These are for you, the reader, to help you understand what is happening.
-	    Feel free to delete them once you know what you're doing, but they should serve as a guide
-	    for when you are first encountering a few different constructs in your Neovim config.
-
-	If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-	I hope you enjoy your Neovim journey,
-	- TJ
-
-	P.S. You can delete this when you're done too. It's your config now! :)
-	--]]
-
+  keymap.set("n", "<space>O", "printf('m`%sO<ESC>``', v:count1)", {
+    expr = true,
+    desc = "insert line above",
+  }
   -- Set <space> as the leader key
   -- See `:help mapleader`
   --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -230,6 +188,7 @@ else
     end,
   })
 
+if not vim.g.vscode then
   -- [[ Install `lazy.nvim` plugin manager ]]
   --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
   local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
